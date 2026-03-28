3daf1459781acee474296a1f49dcae379adcb22e
DavidPontient <d.kayumba1@alustudent.com>
2026-03-28 14:29:28 +0200

    Update dashboard_state.dart

--- lib/presentation/blocs/dashboard_bloc/dashboard_state.dart
+++ lib/presentation/blocs/dashboard_bloc/dashboard_state.dart
@@ -1,19 +1,35 @@
-import '../../../domain/entities/appointment_entity.dart';
+import 'package:equatable/equatable.dart';
+import 'package:ubuzima_connect/domain/entities/appointment_entity.dart';
 
-abstract class DashboardState {}
+abstract class DashboardState extends Equatable {
+  const DashboardState();
 
-class DashboardInitial extends DashboardState {}
+  @override
+  List<Object?> get props => [];
+}
 
-class DashboardLoading extends DashboardState {}
+class DashboardInitial extends DashboardState {
+  const DashboardInitial();
+}
+
+class DashboardLoading extends DashboardState {
+  const DashboardLoading();
+}
 
 class DashboardLoaded extends DashboardState {
   final List<AppointmentEntity> appointments;
 
-  DashboardLoaded(this.appointments);
+  const DashboardLoaded(this.appointments);
+
+  @override
+  List<Object?> get props => [appointments];
 }
 
 class DashboardError extends DashboardState {
   final String message;
 
-  DashboardError(this.message);
+  const DashboardError(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
