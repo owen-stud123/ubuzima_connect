7a80bb7ce83a52ced8d4d692929e64db0c4d29f5
DavidPontient <d.kayumba1@alustudent.com>
2026-03-28 14:29:07 +0200

    Update dashboard_event.dart

--- lib/presentation/blocs/dashboard_bloc/dashboard_event.dart
+++ lib/presentation/blocs/dashboard_bloc/dashboard_event.dart
@@ -1,7 +1,26 @@
-abstract class DashboardEvent {}
+import 'package:equatable/equatable.dart';
+
+abstract class DashboardEvent extends Equatable {
+  const DashboardEvent();
+
+  @override
+  List<Object?> get props => [];
+}
 
 class LoadDashboard extends DashboardEvent {
   final String userId;
 
-  LoadDashboard(this.userId);
+  const LoadDashboard(this.userId);
+
+  @override
+  List<Object?> get props => [userId];
+}
+
+class RefreshDashboard extends DashboardEvent {
+  final String userId;
+
+  const RefreshDashboard(this.userId);
+
+  @override
+  List<Object?> get props => [userId];
 }
